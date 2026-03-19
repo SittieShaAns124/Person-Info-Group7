c4c421d5ac0dfa856a40b34572cfb91b2c20c587
AdapJalaLatiao1296 <jiadap@cmc.pshs.edu.ph>
2026-03-19 10:54:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 name=input("Shahaney? ")
 print("Hello, " + name)
 name = input("Jalaluddin? ")
+age = input("13? )
 addr = input("Marawi City? ")
 print(name + " lives in " + addr)
 year = 2026 - 13 
